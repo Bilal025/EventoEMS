36d35e6318c4111eddf4affac2ced9b48ed9e777
Apsara1505 <prabodi999.desilva@gmail.com>
2023-08-12 14:26:11 +0530

    Order and Payment summary done

---
+++ client/src/pages/OrderSummary.jsx
@@ -0,0 +1,147 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { IoMdArrowBack } from "react-icons/io";
+import { Link, useParams } from 'react-router-dom';
+
+export default function OrderSummary() {
+    const {id} = useParams();
+    const [event, setEvent] = useState(null);
+  
+    useEffect(()=>{
+      if(!id){
+        return;
+      }
+      axios.get(`/event/${id}/ordersummary`).then(response => {
+        setEvent(response.data)
+      }).catch((error) => {
+        console.error("Error fetching events:", error);
+      });
+    }, [id])
+    
+  
+    if (!event) return '';
+    return (
+      <div>
+          <Link to={'/event/'+event._id}>
+          <button 
+              // onClick={handleBackClick}
+              className='
+              inline-flex 
+              mt-12
+              gap-2
+              p-3 
+              ml-12
+              bg-gray-100
+              justify-center 
+              items-center 
+              text-blue-700
+              font-bold
+              rounded-md'
+              >
+          <IoMdArrowBack 
+            className='
+            font-bold
+            w-6
+            h-6
+            gap-2'/> 
+            Back
+          </button>
+          </Link>
+          <div className='flex flex-col'>
+                <div className= 'inline-flex gap-5 mt-8'> 
+                    <div className="
+                      p-4
+                      ml-12 
+                      bg-gray-100
+                      w-3/4
+                      mb-12"
+                      >
+                      <h2
+                          className='
+                            text-left
+                            font-bold
+                            '> 
+                            Terms & Conditions </h2>
+                            <br/>
+  
+                              <div>
+                              <ul>
+                                  <li> Refunds will be provided for ticket cancellations made up to 14 days before the event date. After this period, no refunds will be issued. To request a refund, please contact our customer support team. </li>
+  
+                                  <li> Tickets will be delivered to your registered email address as e-tickets. You can print the e-ticket or show it on your mobile device for entry to the event. </li>
+  
+                                  <li> Each individual is allowed to purchase a maximum of 2 tickets for this event to ensure fair distribution. </li>
+  
+                                  <li> In the rare event of cancellation or postponement, attendees will be notified via email. Refunds will be automatically processed for canceled events. </li>
+  
+                                  <li> Tickets for postponed events will not be refunded and the ticket will be considered a valid ticket on the date of postponement.</li>
+  
+                                  <li> Your privacy is important to us. Our privacy policy outlines how we collect, use, and protect your personal information. By using our app, you agree to our privacy policy. </li>
+  
+                                  <li> Before proceeding with your ticket purchase, please review and accept our terms and conditions, which govern the use of our app and ticketing services. </li>
+                              </ul>
+  
+                              <br/>
+                            </div>
+                                    
+                    </div>
+                    
+                    <div className="
+                      w-1/4
+                      pl-4
+                      h-1/4
+                      mr-12 
+                      bg-blue-100
+                    "
+                      >
+                        <h2 className='
+                            mt-4
+                            font-bold
+                        '>Booking Summary
+                        </h2>
+                        <div className='text-sm flex justify-between' >
+                              <div className='text-left mt-5'>{event.title}</div>
+                              <div className='text-right mt-5 mb-6 pr-5'> {event.ticketPrice}</div>
+                        </div>
+                        
+                        <hr className=" my-2 pt-2 border-gray-300" />
+                        <div className='text-sm font-bold flex justify-between' >
+                          <div className='text-left mt-5'>SUB TOTAL</div>
+                          <div className='text-right mt-5 mb-6 pr-5'>{event.ticketPrice}</div>
+                        </div>
+                       
+                       <input type='checkbox'
+                          
+                          /> I have verified the Event name, date and time before 
+                          proceeding to payment. I accept terms and conditions.
+  
+                        <div className='mb-5'>
+                                  <Link to={'/event/'+event._id+ '/ordersummary'+'/paymentsummary'}>
+                                    <button 
+                                    className='
+                                    mt-5
+                                    p-3 
+                                    ml-2
+                                    w-36
+                                    text-gray-100
+                                    items-center 
+                                    bg-blue-700
+                                    gap-2
+                                    rounded-md'
+                                    // eslint-disable-next-line react/no-unknown-property
+                                    variant="contained" 
+                                    >
+                                      Proceed
+                                    </button>
+                                  </Link>
+                            </div>
+                          
+                            </div>
+                          </div>
+                          
+                          </div> 
+              
+                    </div>
+  
+    );
+}
